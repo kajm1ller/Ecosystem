67507e8fd94737c86194e25d0a1ff6ec6261f497
kajm1ller <kajm@bgsu.edu>
2025-05-10 21:01:05 -0400

    add more private variables

--- animal.h
+++ animal.h
@@ -4,7 +4,8 @@ class animal
 {
 
 public:
-    animal() : health(0), age(0), name("") {};
+    animal() : health(0), age(0), name(""), attackPower(0), energy(0) {};
+    animal(int h, int a, int ap, int e, std::string n) : health(h), age(a), attackPower(ap), energy(e), name(n) {};
     animal(animal &cpy) : health(cpy.health), age(cpy.age), name(cpy.name) {};
     void setHealth(int newHealth) { this->health = newHealth; };
     void setAge(int newAge) { this->age = newAge; };
@@ -12,5 +13,7 @@ public:
 private:
     int health;
     int age;
+    int attackPower;
+    int energy;
     std::string name;
 };
